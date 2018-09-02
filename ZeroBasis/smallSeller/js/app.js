53c19a3b728b28aa24877e08ce18237d76271375
mengyjd <178533869@qq.com>
2018-09-02 16:38:35 +0800

    完成小卖家系列任务

--- ZeroBasis/smallSeller/js/app.js
+++ ZeroBasis/smallSeller/js/app.js
@@ -1,4 +1,10 @@
+drawChart.lines();
+drawChart.bars();
 document.body.onload = function(){
+    if(location.href.split('?')[1]){
+        changeCheckbox(regionCheckbox,'region');
+        changeCheckbox(productCheckbox,'product');
+    }
     drawTable();
 };
 
@@ -7,13 +13,15 @@ radioWrapper.onchange = function(){
     drawChart.lines();
     drawChart.bars();
 };
-tableWrapper.onmouseover = function (e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-    var data = getChartData(target);
-    //drawChart.line(data);
-    //drawChart.bar(data);
-};
+
+// tableWrapper.onmouseover = function (e) {
+//     e = e || window.event;
+//     var target = e.target || e.srcElement;
+//     var data = getChartData(target);
+//     drawChart.line(data);
+//     drawChart.bar(data);
+// };
+
 var saveDataBtn = document.querySelector('#saveData');
 saveDataBtn.onclick = function () {
     saveData();
--- ZeroBasis/smallSeller/js/checkbox.js
+++ ZeroBasis/smallSeller/js/checkbox.js
@@ -77,6 +77,128 @@ function createCheckbox(wrapper, selectObj) {
             if (checkedChildNum === 0) {//不允许取消最后一个选项
                 target.checked = true;
             }
+            // setHash(regionCheckbox, 'region');
+            // setHash(productCheckbox, 'product');
+            addHistoryState(regionCheckbox,'region');
+            addHistoryState(productCheckbox,'product');
+            history.pushState({},null,location.href.split('?')[0] + '?' + JSON.stringify(checkCondition));
         }
     }
 }
+
+
+var checkCondition = {};
+var regionCheckbox = document.querySelectorAll('#region-radio-wrapper input');
+var productCheckbox = document.querySelectorAll('#product-radio-wrapper input');
+
+
+
+/****************           history.State版本            ***************/
+/**
+ * 点击checkbox时push一条history.State
+ * @param checkboxs {object} checkbox对象数组
+ * @param type {string} checkbox类型,可选 region,product
+ */
+function addHistoryState(checkboxs,type) {
+    var checkArray = [];
+    for(var i = 1; i < checkboxs.length; i++) {
+        if (checkboxs[i].checked){
+            checkArray.push(checkboxs[i].value);
+        }
+    }
+    checkCondition[type] = checkArray;
+}
+
+window.addEventListener('popstate',function () {
+    changeCheckbox(regionCheckbox,'region');
+    changeCheckbox(productCheckbox,'product');
+    drawTable();
+});
+
+//解析URL参数返回选择的条件
+function analysisUrl() {
+    if (!location.href.split('?')[1]) return;
+    var condition = location.href.split('?')[1];
+    condition = condition.replace(/%22/g,'\"');
+    condition = decodeURI(condition);
+    condition = JSON.parse(condition);
+    return condition;
+}
+
+/**
+ * 根据url改变checkbox的状态
+ * @param checkboxs {object} checkbox对象数组
+ * @param type {string} checkbox类型,可选 region,product
+ */
+function changeCheckbox(checkboxs,type) {
+    var condition = analysisUrl();
+    for(var i = 1; i < checkboxs.length; i++) {
+        for(var j = 0; j < condition[type].length; j++) {
+            console.log(checkboxs[i].value, condition[type][j]);
+            if (checkboxs[i].value === condition[type][j]){
+                checkboxs[i].checked = true;
+                break;
+            } else {
+                checkboxs[i].checked = false;
+            }
+        }
+    }
+}
+
+
+/****************           location.hash版本            ***************/
+/**
+ * 根据checkbox状态设置location.hash的值
+ * @param {object} wrapper checkbox对象数组
+ * @param {string} type checkbox容器类型 region product
+ */
+// function setHash(wrapper, type) {
+//     var condition = [];
+//     for(var i = 1; i < wrapper.length; i++) {
+//         if (wrapper[i].checked) {
+//             condition.push(wrapper[i].value);
+//         }
+//     }
+//     if (type === 'region'){
+//         checkCondition.region = condition;
+//     } else if(type === 'product') {
+//         checkCondition.product = condition;
+//     }
+//     location.hash = JSON.stringify(checkCondition);
+// }
+
+//changeCheckboxState();
+
+//根据location.hash改变checkbox选中状态
+// function changeCheckboxState() {
+//     if (!location.hash) return;
+//     var parameter = location.hash.replace('#','');
+//     parameter = decodeURI(parameter);
+//     var checkItems = JSON.parse(parameter);
+//     var regions = checkItems.region;
+//     var products = checkItems.product;
+//
+//     for(var i = 1; i < regionCheckbox.length; i++) {
+//         for(var j = 0; j < regions.length; j++) {
+//             if (regionCheckbox[i].value === regions[j]){
+//                 regionCheckbox[i].checked = true;
+//                 break;
+//             } else {
+//                 regionCheckbox[i].checked = false;
+//             }
+//         }
+//     }
+//
+//     for(var m = 1; m < productCheckbox.length; m++) {
+//         for(var n = 0; n < products.length; n++) {
+//             if (productCheckbox[m].value === products[n]){
+//                 productCheckbox[m].checked = true;
+//                 break;
+//             } else {
+//                 productCheckbox[m].checked = false;
+//             }
+//         }
+//     }
+// }
+
+
